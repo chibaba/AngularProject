39dfd014519c4efe8e4305bd029a43a96298f10e
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-02-04 23:48:30 +0100

    addde eventlist component

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,19 +5,30 @@ import { Component, Input,  EventEmitter } from '@angular/core'
 @Component({
     selector: 'event-thumbnail',
     template: `  <div class="well hoverwell thumbnail">
-    <div>Name: {{event.name}}</div>
-    <div>Date: {{event.date}}</div>
-    <div>Time: {{event.time}}</div>
-    <div>Price: \${{event.price}}</div>
+    <div>Name: {{event?.name}}</div>
+    <div>Date: {{event?.date}}</div>
+    <div>Time: {{event?.time}}</div>
+    <div [ngSwitch]="event?.time">
+        <span *ngSwitchCase="'8: 00am'">Early start</span>
+        <span *ngSwitchCase="'10: 00am'">Late Start</span>
+        <span *ngSwitchDefault>Normal Start</span>
+    </div>
+    <div>Price: \${{event?.price}}</div>
+    <div *ngIf="event?.location">
     <div>
-    <span> Location: {{event.location.address}} </span>
-    <span class ="pad-left">{{event.location.city}}, {{event.location.country}}
+    <span> Location: {{event?.location.address}} </span>
+    <span class ="pad-left">{{event?.location.city}}, {{event?.location.country}}
     </span>
+    </div>
+    <div>
+    <div *ngIf="event?.location">
+        Online URL: {{event?.onlineUrl}}
     </div>
    
    </div>
     `,
     styles: [`
+       .thumbnail { min-height: 210px; }
        .pad-left { margin-left: 10px; }
        .well div { color: #bbb; }
     `]
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,12 +6,13 @@ import { Component } from  '@angular/core'
     <div>
      <h2>Upcoming Angular Events</h2>
      <hr/>
-     <div class="well">
-     <div>Hello World</div>
-     </div>
-     <event-thumbnail 
-     [event]="event1"></event-thumbnail>
      
+     <div class="row">
+     <div *ngFor="let events of events" class="col-md-5">
+     <event-thumbnail  
+     [event]="events"></event-thumbnail>
+     </div>
+    </div>
     </div>
     `,
     styles: [`
@@ -22,10 +23,11 @@ import { Component } from  '@angular/core'
 })
 
 export class EventsListComponent {
-   event1 = {
+   events =[ {
        id: 1,
        name: 'Angular metup',
        date: '23/4/2018',
+       Time: '10:00am',
        price: 599.99,
        imageurl: '/assets/images/download(1).jpeg',
        location: {
@@ -33,6 +35,87 @@ export class EventsListComponent {
            city: 'Lagos',
            country: 'England'
        }
-   }
+   },
+   
+   {
+    id: 1,
+    name: 'Angular metup',
+    date: '23/4/2018',
+    price: 599.99,
+    imageurl: '/assets/images/download(1).jpeg',
+    location: {
+        address: 'simpsom street Abuja',
+        city: 'Lagos',
+        country: 'England'
+    }
+},
+{
+    id: 1,
+    name: 'Angular metup',
+    date: '23/4/2018',
+    price: 599.99,
+    imageurl: '/assets/images/download(1).jpeg',
+    location: {
+        address: 'simpsom street Abuja',
+        city: 'Lagos',
+        country: 'England'
+    }
+},
+
+
+
+
+
+    {
+        id: 1,
+        name: 'react metup',
+        date: '23/4/2018',
+        price: 345.99,
+        imageurl: '/assets/images/download(1).jpeg',
+        location: {
+            address: '12 lakwee bodihe',
+            city: 'Lakla',
+            country: 'Angola'
+        }
+    },
+    {
+        id: 1,
+        name: 'java metup',
+        date: '26/4/2018',
+        price: 599.99,
+        imageurl: '/assets/images/download(1).jpeg',
+        location: {
+            address: 'simpsom street Abuja',
+            city: 'Lagos',
+            country: 'Portugal'
+        }
+    },
+    {
+        id: 1,
+        name: 'laravelcast metup',
+        date: '2/4/2018',
+        price: 0.99,
+        imageurl: '/assets/images/download(1).jpeg',
+        location: {
+            address: '16 fountain tower Abuja',
+            city: 'losangeles',
+            country: 'Isand'
+        }
+    },
+    {
+        id: 1,
+        name: 'Bootrap metup',
+        date: '25/4/2018',
+        price: 50.99,
+        imageurl: '/assets/images/download(1).jpeg',
+        location: {
+            address: 'kumar street abiujon',
+            city: 'palandas',
+            country: 'Indiana'
+        }
+    }
+    
+    
+]
    
 }
